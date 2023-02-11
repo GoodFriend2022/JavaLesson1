14c56e05089bdd7a5e8fe6f824512dfe7de05274
Uri_Klevcov <a-08-137.mail.ru>
2023-02-11 23:31:13 +0600

    task2

--- Function.java
+++ Function.java
@@ -1,22 +1,55 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Function {
-    static int Triang(int number){
-        if (number == 0) return number;
+    static int Triang(int number) {
+        if (number == 0)
+            return number;
         number += Triang(number - 1);
         return number;
     }
-    static int Factor(int number){
-        if (number == 0) return 0;
-        if (number == 1) return 1;
+
+    static int Factor(int number) {
+        if (number == 0)
+            return 0;
+        if (number == 1)
+            return 1;
         number *= Factor(number - 1);
         return number;
     }
-    static int userNumber(){
+
+    static int userNumber() {
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите число >> ");
         int number = iScanner.nextInt();
         iScanner.close();
         return number;
+    }
+
+    static ArrayList<Integer> primeNumbers(int maxNumber) {
+        int[] numbers = new int[maxNumber];
+        for (int i = 0; i < maxNumber; i++) {
+            numbers[i] = i + 1;
+        }
+        int count = 0;
+        for (int i = 2; i < numbers.length / 2; i++) {
+            maxNumber -= count;
+            count = 0;
+            int index = 0;
+            boolean item = true;
+            for (int j = i; j < maxNumber; j++) {
+                if (numbers[j] % i == 0) {
+                    if (item) index = j;
+                    item = false;
+                    count++;
+                } 
+                else if (index > 0) numbers[index++] = numbers[j];
+            }
+        }
+        ArrayList<Integer> prime = new ArrayList<Integer>();
+        for (int i = 0; i < maxNumber; i++) {
+            prime.add(numbers[i]);
+        }
+        return prime;
     }
 }
---
+++ Task2.java
@@ -0,0 +1,7 @@
+// Вывести все простые числа от 1 до 1000
+public class Task2 {
+    public static void main(String[] args) {
+        var array = Function.primeNumbers(1000);
+        System.out.print(array);
+    }
+}
