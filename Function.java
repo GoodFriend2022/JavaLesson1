270338fbc6af4b64011b29be92b7f13a99f889a1
Uri_Klevcov <a-08-137.mail.ru>
2023-02-12 01:47:48 +0600

    task3 project

--- Function.java
+++ Function.java
@@ -39,11 +39,12 @@ public class Function {
             boolean item = true;
             for (int j = i; j < maxNumber; j++) {
                 if (numbers[j] % i == 0) {
-                    if (item) index = j;
+                    if (item)
+                        index = j;
                     item = false;
                     count++;
-                } 
-                else if (index > 0) numbers[index++] = numbers[j];
+                } else if (index > 0)
+                    numbers[index++] = numbers[j];
             }
         }
         ArrayList<Integer> prime = new ArrayList<Integer>();
@@ -51,5 +52,59 @@ public class Function {
             prime.add(numbers[i]);
         }
         return prime;
+    }
+
+    static double sum(double x, double y) {
+        return x + y;
+    }
+
+    static double diff(double x, double y) {
+        return x - y;
+    }
+
+    static double mult(double x, double y) {
+        return x * y;
+    }
+
+    static double div(double x, double y) {
+        return x / y;
+    }
+
+    static String userExp() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите выражение для подсчета >> ");
+        String exp = iScanner.nextLine();
+        iScanner.close();
+        return exp;
+    }
+
+    static double Calc(){
+        String expUser = userExp();
+        double res = 0;
+        expUser = expUser.replace("+", " + ");
+        expUser = expUser.replace("-", " - ");
+        expUser = expUser.replace("*", " * ");
+        expUser = expUser.replace("/", " / ");
+        String[] expString = expUser.split(" ");
+        for (int i = 0; i < expString.length; i++) {
+            if (expString[i].compareTo("*") == 0) {
+                double x = Double.parseDouble(expString[i - 1]);
+                double y = Double.parseDouble(expString[i + 1]);
+                res = mult(x, y);
+            } else if (expString[i].compareTo("/") == 0) {
+                double x = Double.parseDouble(expString[i - 1]);
+                double y = Double.parseDouble(expString[i + 1]);
+                res = div(x, y);
+            } else if (expString[i].compareTo("+") == 0) {
+                double x = Double.parseDouble(expString[i - 1]);
+                double y = Double.parseDouble(expString[i + 1]);
+                res = sum(x, y);
+            } else if (expString[i].compareTo("-") == 0) {
+                double x = Double.parseDouble(expString[i - 1]);
+                double y = Double.parseDouble(expString[i + 1]);
+                res = diff(x, y);
+            }
+        }
+        return res;
     }
 }
---
+++ Task3.java
@@ -0,0 +1,6 @@
+// Реализовать простой калькулятор
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(Function.Calc());
+    }
+}
