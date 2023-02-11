49a34ee528e1f33ae7b5630028f771928cfea4de
Uri_Klevcov <a-08-137.mail.ru>
2023-02-11 21:57:25 +0600

    task1

--- Task1.java
+++ Task1.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
+// Вычислить n-ое треугольное число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
 public class Task1 {
-    // Вычислить n-ое треугольное число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
     public static void main(String[] args) {
         int n = Function.userNumber();
         System.out.printf("Треугольное число от %d = %d\n", n, Function.Triang(n));
